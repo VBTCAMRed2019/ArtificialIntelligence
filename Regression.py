3a809d124307ff1eb9e36b7a34e97b20647b0447
VBTCAMRed2019 <46923748+VBTCAMRed2019@users.noreply.github.com>
2019-05-15 08:56:04 -0400

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -9,29 +9,26 @@ from math import *
 ## gradient is worked out using  (ysub1 - ysub2) / (xsub1 - xsub2)
 ## c = y - mx
 ##
+
 x1 = 1
 x2 = 2
 
 print("I am an artificial intelligence that will calculate the correlation between sets of data. I will tell you x values and you put in coreesponding y values.\n")
 print("X Values\tY Values \n", x1, "\n", x2)
 
-y1 = int(input("\n Enter the first y value"))
+y1 = int(input("\n Enter the first y value")) ##Ask for the first corresponding y value
 
-print("\nX Values\tY Values \n", x1, "\t\t", y1, "\n", x2)
+print("\nX Values\tY Values \n", x1, "\t\t", y1, "\n", x2) ##print out their input into the chart
 
-y2 = int(input("\n Enter the the second y value"))
+y2 = int(input("\n Enter the the second y value")) ##Ask for the second corresponding y value
 
-print("\nX Values\tY Values \n", x1, "\t\t", y1, "\n", x2, "\t\t", y2)
+print("\nX Values\tY Values \n", x1, "\t\t", y1, "\n", x2, "\t\t", y2) ##print out their input into the chart
 
-##Calculate the slope
-slope = ((y1 - y2) / (x1 - x2))
+slope = ((y1 - y2) / (x1 - x2))##Calculate the slope
 
-##solve y-intercept
-intercept = (y1 - slope)
+intercept = (y1 - slope) ##Calculate the intercept
 
 
-print ("When x = 3, y would equal", (slope * 3 + intercept))
-##changing it up
-
+print ("When x = 3, y would equal", (slope * 3 + intercept)) ##Print what ysub3 woudl be when xsub3 = 3
 
 
